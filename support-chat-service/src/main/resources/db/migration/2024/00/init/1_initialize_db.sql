02656c5edffd26e0d51c801d6d23d45e28c3b74f
Alexander Bulatov <abulatov@nota.tech>
2024-11-01 19:07:18 +0300

    feat: prepare schema

--- support-chat-service/src/main/resources/db/migration/2024/00/init/1_initialize_db.sql
+++ support-chat-service/src/main/resources/db/migration/2024/00/init/1_initialize_db.sql
@@ -1,8 +1,26 @@
-CREATE SCHEMA IF NOT EXISTS entities_management;
+CREATE SCHEMA IF NOT EXISTS users;
 
-CREATE SEQUENCE IF NOT EXISTS entities_management.model_id_seq START WITH 1 INCREMENT BY 1;
+CREATE SCHEMA IF NOT EXISTS chat;
 
-CREATE TABLE IF NOT EXISTS entity (
+CREATE SEQUENCE IF NOT EXISTS chat.message_id_seq START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE IF NOT EXISTS users.users
+(
     id BIGINT,
-    CONSTRAINT pk_entity PRIMARY KEY (id)
+    email VARCHAR(20),
+    CONSTRAINT pk_users PRIMARY KEY (id),
+    CONSTRAINT uq_email UNIQUE (email)
 );
+
+CREATE TABLE IF NOT EXISTS chat.message
+(
+    id        BIGINT,
+    text      VARCHAR(1000) NOT NULL,
+    timestamp timestamptz,
+    replied_to BIGINT,
+    CONSTRAINT pk_message PRIMARY KEY (id),
+    CONSTRAINT not_replied_to_itself CHECK (id <> message.replied_to)
+);
+
+ALTER TABLE chat.message
+    ADD CONSTRAINT fk_replied_to FOREIGN KEY (replied_to) REFERENCES chat.message (id);
