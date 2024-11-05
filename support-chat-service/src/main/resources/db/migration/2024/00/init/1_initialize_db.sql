d5ff13e8d67284853cd4949af0f6922c919db1c6
Alexander Bulatov <abulatov@nota.tech>
2024-11-05 18:30:42 +0300

    feat: add author_id field to chat.message table and fk constraint, add formatting

--- support-chat-service/src/main/resources/db/migration/2024/00/init/1_initialize_db.sql
+++ support-chat-service/src/main/resources/db/migration/2024/00/init/1_initialize_db.sql
@@ -6,7 +6,7 @@ CREATE SEQUENCE IF NOT EXISTS chat.message_id_seq START WITH 1 INCREMENT BY 1;
 
 CREATE TABLE IF NOT EXISTS users.users
 (
-    id BIGINT,
+    id    BIGINT,
     email VARCHAR(20),
     CONSTRAINT pk_users PRIMARY KEY (id),
     CONSTRAINT uq_email UNIQUE (email)
@@ -14,13 +14,17 @@ CREATE TABLE IF NOT EXISTS users.users
 
 CREATE TABLE IF NOT EXISTS chat.message
 (
-    id        BIGINT,
-    text      VARCHAR(1000) NOT NULL,
-    timestamp timestamptz,
+    id         BIGINT,
+    text       VARCHAR(1000) NOT NULL,
+    timestamp  timestamptz,
     replied_to BIGINT,
+    author_id  BIGINT,
     CONSTRAINT pk_message PRIMARY KEY (id),
     CONSTRAINT not_replied_to_itself CHECK (id <> message.replied_to)
 );
 
 ALTER TABLE chat.message
     ADD CONSTRAINT fk_replied_to FOREIGN KEY (replied_to) REFERENCES chat.message (id);
+
+ALTER TABLE chat.message
+    ADD CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES users.users (id);
