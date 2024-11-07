130ae92bd8db0759f4672d575c1df2316968b80e
Alexander Bulatov <abulatov@nota.tech>
2024-11-07 17:54:51 +0300

    feat: rename template app class

--- support-chat-service/src/main/java/ru/expanse/SupportChatApplication.java
+++ support-chat-service/src/main/java/ru/expanse/SupportChatApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EntityApplication {
+public class SupportChatApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EntityApplication.class, args);
+        SpringApplication.run(SupportChatApplication.class, args);
     }
 }
