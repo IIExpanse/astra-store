0fa6d8b2089ee10039dde9a1a64b26c619a01ac6
Alexander Bulatov <abulatov@nota.tech>
2024-11-07 17:53:31 +0300

    feat: add update method to mapper

--- support-chat-service/src/main/java/ru/expanse/mapper/MessageMapper.java
+++ support-chat-service/src/main/java/ru/expanse/mapper/MessageMapper.java
@@ -1,11 +1,15 @@
 package ru.expanse.mapper;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import ru.expanse.model.Message;
 import ru.expanse.model.User;
 import ru.expanse.schema.MessageRecord;
+import ru.expanse.schema.UpdateMessageRequest;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING, uses = BaseTypesMapper.class)
 public interface MessageMapper {
@@ -13,4 +17,7 @@ public interface MessageMapper {
 
     @Mapping(target = "authorId", source = "message.author.id")
     MessageRecord toRecord(Message message);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Message updateModel(UpdateMessageRequest request, @MappingTarget Message message);
 }
