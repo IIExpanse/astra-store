c81837b3549b983b52ea746acecc510116a2ab2a
Alexander Bulatov <abulatov@nota.tech>
2024-11-12 17:19:10 +0300

    feat: add exception codes for chat user and chat user association entities

--- support-chat-service/src/main/java/ru/expanse/exception/ExceptionCode.java
+++ support-chat-service/src/main/java/ru/expanse/exception/ExceptionCode.java
@@ -4,8 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionCode {
+    CHAT_NOT_FOUND("chat not found"),
     MESSAGE_NOT_FOUND("message not found"),
-    USER_NOT_FOUND("user not found");
+    USER_NOT_FOUND("user not found"),
+    CHAT_USER_ASSOCIATION_NOT_FOUND("chat user association not found"),;
 
     private final String message;
 
