7079a384594e8a3dacd92aa046a42bf68468c9b8
Alexander Bulatov <abulatov@nota.tech>
2024-11-12 17:27:20 +0300

    fix: fix test after changes

--- support-chat-service/src/test/java/ru/expanse/dao/adapter/MessageDaoAdapterTest.java
+++ support-chat-service/src/test/java/ru/expanse/dao/adapter/MessageDaoAdapterTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import ru.expanse.dao.repository.ChatRepository;
 import ru.expanse.dao.repository.UserRepository;
+import ru.expanse.model.Chat;
 import ru.expanse.model.Message;
 import ru.expanse.model.User;
 import ru.expanse.schema.UpdateMessageRequest;
@@ -20,6 +22,8 @@ class MessageDaoAdapterTest {
     private MessageDaoAdapter messageDaoAdapter;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ChatRepository chatRepository;
 
     @Nested
     class CrudTest {
@@ -48,12 +52,18 @@ class MessageDaoAdapterTest {
 
     private Message saveDefaultMessage() {
         User user = saveDefaultUser();
-        Message message = DataProvider.getMessage(user);
+        Chat chat = saveDefaultChat();
+        Message message = DataProvider.getMessage(user, chat);
         return messageDaoAdapter.save(message);
     }
 
     private User saveDefaultUser() {
         User user = DataProvider.getUser();
         return userRepository.save(user);
+    }
+
+    private Chat saveDefaultChat() {
+        Chat chat = DataProvider.getChat();
+        return chatRepository.save(chat);
     }
 }
--- support-chat-service/src/test/java/ru/expanse/util/DataProvider.java
+++ support-chat-service/src/test/java/ru/expanse/util/DataProvider.java
@@ -1,16 +1,18 @@
 package ru.expanse.util;
 
+import ru.expanse.model.Chat;
 import ru.expanse.model.Message;
 import ru.expanse.model.User;
 
 import java.time.OffsetDateTime;
 
 public class DataProvider {
-    public static Message getMessage(User user) {
+    public static Message getMessage(User user, Chat chat) {
         Message message = new Message();
         message.setText("Some chat message");
-        message.setAuthor(user);
         message.setTimestamp(OffsetDateTime.now());
+        message.setAuthor(user);
+        message.setChat(chat);
         return message;
     }
 
@@ -18,5 +20,11 @@ public class DataProvider {
         User user = new User();
         user.setEmail("some-mail@mail.com");
         return user;
+    }
+
+    public static Chat getChat() {
+        Chat chat = new Chat();
+        chat.setName("chat_name");
+        return chat;
     }
 }
