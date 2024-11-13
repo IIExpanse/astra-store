08da363115db5ab37861f1492cc0156480a45116
Alexander Bulatov <abulatov@nota.tech>
2024-11-13 18:14:37 +0300

    feat: add validator annotations, change timestamp type

--- support-chat-service/src/main/java/ru/expanse/schema/MessageRecord.java
+++ support-chat-service/src/main/java/ru/expanse/schema/MessageRecord.java
@@ -1,6 +1,15 @@
 package ru.expanse.schema;
 
-import java.time.ZonedDateTime;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record MessageRecord(Long id, String text, ZonedDateTime timestamp, Long repliedTo, Long authorId) {
+import java.time.OffsetDateTime;
+
+public record MessageRecord(
+        @NotNull Long id,
+        @NotBlank String text,
+        @NotNull OffsetDateTime timestamp,
+        Long repliedTo,
+        @NotNull Long authorId
+) {
 }
