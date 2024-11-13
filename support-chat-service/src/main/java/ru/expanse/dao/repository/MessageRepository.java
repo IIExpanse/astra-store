336307dacc42f0e43915d37ae6f0da2003ca855a
Alexander Bulatov <abulatov@nota.tech>
2024-11-13 18:04:33 +0300

    feat: add method for retrieving message collection filtered by timestamp range

--- support-chat-service/src/main/java/ru/expanse/dao/repository/MessageRepository.java
+++ support-chat-service/src/main/java/ru/expanse/dao/repository/MessageRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.expanse.model.Message;
 
+import java.time.OffsetDateTime;
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findAllByTimestampGreaterThanEqualAndTimestampLessThanEqual(OffsetDateTime start, OffsetDateTime end);
 }
