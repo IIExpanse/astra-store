505f3b29ead4006d4201468249d0aae21b499ce2
Alexander Bulatov <abulatov@nota.tech>
2024-11-20 19:49:20 +0300

    feat: add test for postMessage websocket endpoint

--- support-chat-service/src/test/java/ru/expanse/controller/MessageControllerTest.java
+++ support-chat-service/src/test/java/ru/expanse/controller/MessageControllerTest.java
@@ -2,24 +2,44 @@ package ru.expanse.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 import ru.expanse.schema.GetAllMessagesRequest;
+import ru.expanse.schema.MessageAction;
+import ru.expanse.schema.MessageEvent;
 import ru.expanse.schema.MessageRecord;
+import ru.expanse.schema.SaveMessageRequest;
 import ru.expanse.service.MessageService;
 import ru.expanse.util.DataProvider;
 
+import java.lang.reflect.Type;
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -27,7 +47,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class MessageControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -35,6 +54,8 @@ class MessageControllerTest {
     private ObjectMapper objectMapper;
     @MockBean
     private MessageService messageService;
+    @LocalServerPort
+    private Integer port;
 
     @Nested
     class RestTest {
@@ -73,9 +94,40 @@ class MessageControllerTest {
 
     @Nested
     class WebSocketTest {
-        @Test
-        void postMessage() {
+        private BlockingQueue<MessageEvent> blockingQueue;
+        private WebSocketStompClient stompClient;
+        private StompSession session;
 
+        @BeforeEach
+        @SneakyThrows
+        public void setup() {
+
+            blockingQueue = new LinkedBlockingDeque<>();
+            stompClient = new WebSocketStompClient(new SockJsClient(
+                    List.of(new WebSocketTransport(new StandardWebSocketClient()))));
+
+            stompClient.setMessageConverter(new MappingJackson2MessageConverter(objectMapper));
+            session = stompClient.connectAsync(getWsPath(), new DefaultStompSessionHandler())
+                    .get(1, TimeUnit.SECONDS);
+        }
+
+        @AfterEach
+        public void disconnect() {
+            session.disconnect();
+        }
+
+        @Test
+        @SneakyThrows
+        void postMessage() {
+            MessageEvent event = new MessageEvent(1L, 2L, MessageAction.CREATE);
+            Mockito.when(messageService.saveMessage(ArgumentMatchers.any(SaveMessageRequest.class)))
+                            .thenReturn(event);
+            session.subscribe(MessageController.EVENTS_TOPIC, new DefaultStompSessionHandler());
+
+            SaveMessageRequest request = DataProvider.getDefaultSaveMessageRequest();
+
+            session.send("/ws-request/message/new", request);
+            assertEquals(event, blockingQueue.poll(1, TimeUnit.SECONDS));
         }
 
         @Test
@@ -84,6 +136,27 @@ class MessageControllerTest {
 
         @Test
         void deleteMessage() {
+        }
+
+        class DefaultStompSessionHandler extends StompSessionHandlerAdapter {
+            @Override
+            public Type getPayloadType(StompHeaders stompHeaders) {
+                return MessageEvent.class;
+            }
+
+            @Override
+            public void handleFrame(StompHeaders stompHeaders, Object o) {
+                blockingQueue.offer((MessageEvent) o);
+            }
+
+            @Override
+            public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload, Throwable exception) {
+                exception.printStackTrace();
+            }
+        }
+
+        private String getWsPath() {
+            return String.format("ws://localhost:%d/ws", port);
         }
     }
 }
