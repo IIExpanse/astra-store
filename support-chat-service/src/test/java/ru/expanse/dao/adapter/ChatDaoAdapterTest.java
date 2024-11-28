cbe17485af623930b5f5e726744354baf68daad5
Alexander Bulatov <abulatov@nota.tech>
2024-11-28 18:07:26 +0300

    feat: add test for chats filter method

--- support-chat-service/src/test/java/ru/expanse/dao/adapter/ChatDaoAdapterTest.java
+++ support-chat-service/src/test/java/ru/expanse/dao/adapter/ChatDaoAdapterTest.java
@@ -5,8 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import ru.expanse.dao.repository.ChatUserRepository;
+import ru.expanse.dao.repository.UserRepository;
 import ru.expanse.model.Chat;
+import ru.expanse.model.ChatUser;
+import ru.expanse.model.ChatUserId;
+import ru.expanse.model.User;
 import ru.expanse.util.DataProvider;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,15 +21,52 @@ import static org.junit.jupiter.api.Assertions.*;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ChatDaoAdapterTest {
     @Autowired
-    private ChatDaoAdapter chatRepository;
+    private ChatDaoAdapter userDaoAdapter;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private ChatUserRepository chatUserRepository;
+    @Autowired
+    private ChatDaoAdapter chatDaoAdapter;
 
     @Nested
     class CrudTest {
         @Test
         void saveAndGet() {
             Chat chat = DataProvider.getDefaultChat();
-            chatRepository.save(chat);
-            assertTrue(chatRepository.getById(chat.getId()).isPresent());
+            userDaoAdapter.save(chat);
+            assertTrue(userDaoAdapter.getById(chat.getId()).isPresent());
+        }
+    }
+
+    @Nested
+    class CustomLogicTest {
+        @Test
+        void getChatsByFilter() {
+            User user = DataProvider.getDefaultUser();
+            user = userRepository.save(user);
+
+            Chat chat1 = userDaoAdapter.save(DataProvider.getDefaultChat());
+            ChatUserId chatUserId1 = new ChatUserId();
+            chatUserId1.setUser(user);
+            chatUserId1.setChat(chat1);
+            ChatUser chatUser1 = DataProvider.getDefaultChatUser(chatUserId1);
+            chatUserRepository.save(chatUser1);
+
+            Chat chat2 = userDaoAdapter.save(DataProvider.getDefaultChat());
+            ChatUserId chatUserId2 = new ChatUserId();
+            chatUserId2.setUser(user);
+            chatUserId2.setChat(chat2);
+            ChatUser chatUser2 = DataProvider.getDefaultChatUser(chatUserId2);
+            chatUserRepository.save(chatUser2);
+
+            assertEquals(2, chatDaoAdapter.getChatsByFilter(null, null).size());
+            assertEquals(2, userDaoAdapter.getChatsByFilter(user.getId(), null).size());
+            assertEquals(1, userDaoAdapter.getChatsByFilter(user.getId(), List.of(chat1.getId())).size());
+            assertEquals(1, userDaoAdapter.getChatsByFilter(null, List.of(chat1.getId())).size());
+            assertEquals(0, userDaoAdapter.getChatsByFilter(Long.MAX_VALUE, null).size());
+            assertEquals(0, userDaoAdapter.getChatsByFilter(null, List.of()).size());
+            assertEquals(0, userDaoAdapter.getChatsByFilter(null, List.of(Long.MAX_VALUE)).size());
         }
     }
 }
