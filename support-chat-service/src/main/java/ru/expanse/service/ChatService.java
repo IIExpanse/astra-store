1680c2a58ce48a54522f39998c136b6bbd3b2a3c
Alexander Bulatov <abulatov@nota.tech>
2024-11-29 19:15:33 +0300

    feat: change method to return chat-specific messages

--- support-chat-service/src/main/java/ru/expanse/service/ChatService.java
+++ support-chat-service/src/main/java/ru/expanse/service/ChatService.java
@@ -5,14 +5,12 @@ import ru.expanse.schema.ChatRecord;
 import ru.expanse.schema.SaveChatRequest;
 
 import java.time.OffsetDateTime;
-import java.util.List;
 
 public interface ChatService {
     ChatEvent saveChat(SaveChatRequest request);
 
-    ChatRecord getChatsByFilter(
-            Long userId,
-            List<Long> chatIds,
+    ChatRecord getChatWithMessages(
+            Long chatId,
             OffsetDateTime messagesFrom,
             OffsetDateTime messagesTo
     );
